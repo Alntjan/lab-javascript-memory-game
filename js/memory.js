837425f7efe17d24bc9616f238bf4edcf023fc0f
UmaCouve <lourencokramerhorta@gmail.com>
2020-11-19 19:29:27 +0000

    done

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,40 @@
-class MemoryGame {
-  constructor(cards){
-    this.cards = cards;
-    // add the rest of the class properties here
+const fisherYatesShuffle = (cards) => {
+  let currentIndex = cards.length;
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    let temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  return cards;
+};
+
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
+
+  shuffleCards() {
+    this.cards = fisherYatesShuffle(this.cards);
+  }
+  checkIfPair(card1Name, card2Name) {
+    this.pairsClicked += 1;
+    if (card1Name === card2Name) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
